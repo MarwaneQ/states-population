8f68152fb39639400e1b014a64e3d6547f00aa5f
MarwaneQ <merouane12345bensadallah@gmail.com>
2023-12-29 09:00:44 -0800

    js

--- index.js
+++ index.js
@@ -3,3 +3,29 @@ const cities = []
 fetch(endpoint)
 .then(blob => blob.json())
 .then(data => cities.push(...data))
+
+function findMatch(wordToMatch,cities) {
+    return cities.filter(place =>{
+
+        let regex = new RegExp(wordToMatch,'gi')
+        return place.city.match(regex) || place.state.match(regex)
+    })
+}
+function displayMatches() {
+    let matchArray = findMatch(this.value,cities)
+    let html = matchArray.map(place =>{
+        let regex = new RegExp(this.value,'gi')
+        let cityNmae = place.city.replace(regex,`<span class="hi">${this.value}</span>`)
+        return `
+        <li>
+            <span class="name">${cityNmae},${place.state}</span>
+            <span class="population">${place.population}</span>
+        </li>
+        `
+    }).join('')
+    suggestion.innerHTML = html;
+}
+let searchInput = document.querySelector('.search')
+let suggestion = document.querySelector('.suggestion')
+searchInput.addEventListener('change',displayMatches)
+searchInput.addEventListener('keyup',displayMatches)
